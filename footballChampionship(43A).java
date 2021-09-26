45ccc9068389f61ef9c048751ae5fd884d678aad
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-09-25 20:19:55 -0500

    Descriptions added

--- footballChampionship(43A).java
+++ footballChampionship(43A).java
@@ -5,13 +5,17 @@ public class footballChampionship {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        // By default we give the first team a point since there is a guaranteed winner.
         int t1 = 1;
         int t2 = 0;
 
+        // Taking first team name and declaring variables to store/compare subsequent names
         String a = br.readLine();
         String c;
         String b = "";
 
+        // Iterating n-1 times, comparing new team name with known team.
+        // We then keep track of team scores from here until loop is finished.
         for ( int i = 1; i < n; i++ ) {
             c = br.readLine();
             if ( c.equals(a) ) {
@@ -22,6 +26,7 @@ public class footballChampionship {
             }
         }
 
+        // Printing team with highest score. Ez pz.
         if ( t1 > t2 ) {
             System.out.println(a);
         } else {
