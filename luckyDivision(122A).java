3de01cb7d4508a52d5812c535a89229cd5e47b21
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-09-25 20:26:28 -0500

    Descriptions added

--- luckyDivision(122A).java
+++ luckyDivision(122A).java
@@ -1,3 +1,6 @@
+// I don't honestly believe that this is the most elegant way of solving this problem but it certainly works and I think it's easy to follow.
+
+
 import java.io.*;
 // 122A
 public class luckyDivision {
@@ -6,14 +9,15 @@ public class luckyDivision {
         String input = br.readLine();
         boolean luck = true;
 
-        // All possible lucky combos under 1000 as 1<n<1000
+        // Checking all possible lucky multiples under 1000 as 1<n<1000
         if ( Integer.parseInt(input) % 4 == 0 || Integer.parseInt(input) % 7 == 0 || Integer.parseInt(input) % 47 == 0 || Integer.parseInt(input) % 74 == 0 ) {
             //continues
         } else {
-            // checking for a purely lucky number series of digits
+            // checking for a purely lucky number series of digits. If false, end loop and save some time.
             for ( int i = 0; i < input.length(); i++ ) {
                 if ( input.charAt(i) != '4' && input.charAt(i) != '7' ) {
                     luck = false;
+                    break;
                 }
             }
         }
