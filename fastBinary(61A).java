19dec206a5c61ac10b5e0c09a9744d0d04786ffd
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-09-25 20:12:58 -0500

    Descriptions added

--- fastBinary(61A).java
+++ fastBinary(61A).java
@@ -2,7 +2,7 @@
 This is the solution to the CodeForces 61A challenge. This challenge gives the user two lines of '1' and '0' characters of length 1 <= n <= 100.
 The user needs to sort them where any two similar characters that fall into the same index become '0', and any index where characters differ between
 the two strings will become '1'.
-It is a very easy problem and can be solved with two check statements.
+It is a very easy problem and can be solved with a single loop and check.
 */
 
 
@@ -13,10 +13,13 @@ import java.io.IOException;
 public class fastBinary {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Let's take input as a string since any leading zeros will cause all sorts of problems if we use integers
         String lineOne = br.readLine();
         String lineTwo = br.readLine();
         String result = "";
 
+        // Comparing each index of the two strings, and adding the appropiate character to the result string.
         for ( int i = 0; i < lineOne.length(); i++ ) {
             if ( lineOne.charAt(i) == lineTwo.charAt(i) ) {
                 result += '0';
